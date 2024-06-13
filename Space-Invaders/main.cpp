27e843c04624c7ab6529439be1889cc95cc6608b
Abhaymodi860 <modiabhay860@gmail.com>
2024-06-13 13:32:09 +0530

    player class added

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,4 +1,35 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+class Player
+{
+  private: 
+      // private properties
+      int health = 3;
+      sf::Vector2f position = sf::Vector2f(200.0f, 100.0f);
+      int player_score = 0;
+      int movement_speed = 5;
+
+  public:
+      // public properties
+      sf::Texture player_texture;
+      sf::Sprite player_sprite;
+
+      // public set and get function
+      int getScore()
+      {
+          return player_score;
+      }
+
+      void setScore(int newScore)
+      {
+          player_score = newScore;
+      }
+
+      void takeDamage() {};
+      void move() {};
+      void shootBullets() {};
+};
 
 int main()
 {
@@ -6,7 +37,7 @@ int main()
     sf::VideoMode videoMode = *(new sf::VideoMode(800, 600));
 
     //create window object with specific dimensions and title
-    sf::RenderWindow* window = new sf::RenderWindow(videoMode, "My SFML Window");
+    sf::RenderWindow* window = new sf::RenderWindow(videoMode, "Space Invaders");
 
     // game loop
     while (window->isOpen()) {
@@ -18,58 +49,23 @@ int main()
         }
 
         // clear window
-        window->clear(sf::Color::Yellow);
+        window->clear(sf::Color::Blue);
 
         // Draw Content here
         //-------------------
-        // Draw circle
-        sf::CircleShape circle(50);  // radius 50
-        circle.setFillColor(sf::Color::Green);
-        circle.setPosition(400, 300); // set position (x, y)
-        window->draw(circle);
 
-        // Create a rectangle shape
-        sf::RectangleShape rectangle(sf::Vector2f(100, 100));
-
-        // Set the position of the rectangle
-        rectangle.setPosition(10, 10);
-
-        rectangle.setFillColor(sf::Color::Red);
-
-        rectangle.setOutlineColor(sf::Color::Magenta);
-        rectangle.setOutlineThickness(5);
-        window->draw(rectangle);
-
-        sf::CircleShape Triangle(50, 3);  // radius 50
-        Triangle.setFillColor(sf::Color::Black);
-        Triangle.setPosition(700, 300); // set position (x, y)
-        window->draw(Triangle);
-
-        // displaying an image
-        sf::Texture outscal_texture;
-        outscal_texture.loadFromFile("assets/textures/outscal_logo.png");
-
-        sf::Sprite outscal_sprite;
-        outscal_sprite.setTexture(outscal_texture);
-
-        outscal_sprite.setPosition(200, 100);
-        outscal_sprite.setRotation(45);
-        outscal_sprite.setScale(0.5, 0.5);
-       
-        window->draw(outscal_sprite);
-
-        // displaying text
-        sf::Font font;
-        font.loadFromFile("assets/fonts/OpenSans.ttf");
-        sf::Text text("SFML is awesome!", font, 50);
-        text.setFillColor(sf::Color::Red);
-        text.setPosition(300, 200);
-        window->draw(text);
 
         // ------------------
      
         //displat the content
         window->display();
+
+        Player player;
+        std::cout << "\nPlayer Score: " << player.getScore() << "\n";
+
+        player.setScore(100);
+
+        std::cout << "Player modified score: " << player.getScore() << "\n";
     }
 
     return 0;
